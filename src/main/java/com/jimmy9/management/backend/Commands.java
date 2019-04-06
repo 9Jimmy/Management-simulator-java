d571b35f9f858cbd9f19c074bb989ce29d094f20
9Jimmy <9jimmyw@gmail.com>
2019-04-06 16:37:36 +0500

    management-simulator-v3.6

--- src/main/java/com/jimmy9/management/backend/Commands.java
+++ src/main/java/com/jimmy9/management/backend/Commands.java
@@ -2,12 +2,53 @@ package com.jimmy9.management.backend;
 
 import java.util.Scanner;
 
- class Commands {
+import static java.lang.System.out;
+
+class Commands {
     private Company companyObj = new Company();
     private Scanner input = new Scanner(System.in);
 
+    private String company, key, fname, sname, position, age, salary, value;
+
+    private void checkStringValue(String value){
+        String a = input.nextLine();
+
+        while (a.equals("")){
+            out.println("Cannot be empty. Try again.");
+            out.print("-> ");
+            a = input.nextLine();
+        }
+
+        switch (value){
+            case "company":
+                this.company = a;
+                break;
+            case "key":
+                this.key = a;
+                break;
+            case "fname":
+                this.fname = a;
+                break;
+            case "sname":
+                this.sname = a;
+                break;
+            case "position":
+                this.position = a;
+                break;
+            case "age":
+                this.age = a;
+                break;
+            case "salary":
+                this.salary = a;
+                break;
+            case "value":
+                this.value = a;
+                break;
+        }
+    }
+
      void info(){
-        System.out.println("\n\t░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█\n" +
+        out.println("\n\t░░░░░░░█▐▓▓░████▄▄▄█▀▄▓▓▓▌█\n" +
                 "\t░░░░░▄█▌▀▄▓▓▄▄▄▄▀▀▀▄▓▓▓▓▓▌█\n" +
                 "\t░░░▄█▀▀▄▓█▓▓▓▓▓▓▓▓▓▓▓▓▀░▓▌█\n" +
                 "\t░░█▀▄▓▓▓███▓▓▓███▓▓▓▄░░▄▓▐█▌ \n" +
@@ -18,82 +59,92 @@ import java.util.Scanner;
                 "\t▌▓▄▌▀░▀░▐▀█▄▓▓██████████▓▓▓▌█▌\n" +
                 "\t▌▓▓▓▄▄▀▀▓▓▓▀▓▓▓▓▓▓▓▓█▓█▓█▓▓▌█▌ \n" +
                 "\t█▐▓▓▓▓▓▓▄▄▄▓▓▓▓▓▓█▓█▓█▓█▓▓▓▐█\n");
-        System.out.println("\tManagement-Simulator\n\tCreated by 9Jimmy\n\t03.03.2019");
+        out.println("\tManagement-Simulator\n\tCreated by 9Jimmy\n\t03.03.2019");
     }
 
      void help(){
-        System.out.println("***************************************************************************");
-        System.out.println("*\tList of commands:                                                     *\n*                                                                         *\n" +
+        out.println("***************************************************************************");
+        out.println("*\tList of commands:                                                     *\n*" +
+                "                                                                         *\n" +
                 "*\t1)'addW'-Add new employee (If the specified company                   " +
-                "*                                                                         *\n*\tdoes not exist, a new one will be created with the specified name).   *\n" +
-                "*                                                                         *\n*\t2)'showAll'-show list of all employees in selected company.           *\n" +
-                "*                                                                         *\n*\t3)'showS'-show selected employee in selected company.                 *\n" +
-                "*                                                                         *\n*\t4)'changeS'-change salary of specified employee.                      *\n" +
-                "*                                                                         *\n*\t5)'changeP'-change position of specified employee.                    *\n" +
-                "*                                                                         *\n*\t6)'deleteW'-delete specified employee.                                *\n" +
-                "*                                                                         *\n*\t7)'clist'-show list of all companies.                                 *\n" +
-                "*                                                                         *\n*\t8)'exit'-exit.                                                        *\n*                                                                         *");
-        System.out.println("***************************************************************************");
+                "*                                                                         " +
+                "*\n*\tdoes not exist, a new one will be created with the specified name).   *\n" +
+                "*                                                                         " +
+                "*\n*\t2)'showAll'-show list of all employees in selected company.           *\n" +
+                "*                                                                         " +
+                "*\n*\t3)'showS'-show selected employee in selected company.                 *\n" +
+                "*                                                                         " +
+                "*\n*\t4)'changeS'-change salary of specified employee.                      *\n" +
+                "*                                                                         " +
+                "*\n*\t5)'changeP'-change position of specified employee.                    *\n" +
+                "*                                                                         " +
+                "*\n*\t6)'deleteW'-delete specified employee.                                *\n" +
+                "*                                                                         " +
+                "*\n*\t7)'clist'-show list of all companies.                                 *\n" +
+                "*                                                                         " +
+                "*\n*\t8)'exit'-exit.                                                        *\n*" +
+                "                                                                         *");
+        out.println("***************************************************************************");
     }
 
      void addW(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
-        System.out.print("Enter key of employee (must be unique): ");
-        String key = input.nextLine();
-        System.out.print("Enter first name of employee: ");
-        String fname = input.nextLine();
-        System.out.print("Enter second name of employee: ");
-        String sname = input.nextLine();
-        System.out.print("Enter position of employee: ");
-        String position = input.nextLine();
-        System.out.print("Enter age of employee: ");
-        String age = input.nextLine();
-        System.out.print("Enter salary of employee: ");
-        String salary = input.nextLine();
+        out.print("Enter company name: ");
+        checkStringValue("company");
+        out.print("Enter key of employee (must be unique): ");
+        checkStringValue("key");
+        out.print("Enter first name of employee: ");
+        checkStringValue("fname");
+        out.print("Enter second name of employee: ");
+        checkStringValue("sname");
+        out.print("Enter position of employee: ");
+        checkStringValue("position");
+        out.print("Enter age of employee: ");
+        checkStringValue("age");
+        out.print("Enter salary of employee: ");
+        checkStringValue("salary");
         companyObj.addWorker(company, key, fname, sname, position, age, salary);
     }
 
     void showAll(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
+        out.print("Enter company name: ");
+        checkStringValue("company");
         companyObj.showListOfWorkersInSelectedCompany(company);
     }
 
     void showS(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
-        System.out.print("Enter key of employee: ");
-        String key = input.nextLine();
+        out.print("Enter company name: ");
+        checkStringValue("company");
+        out.print("Enter key of employee: ");
+        checkStringValue("key");
         companyObj.showSelectedWorker(company, key);
     }
 
     void changeS(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
-        System.out.print("Enter key of employee: ");
-        String key = input.nextLine();
-        System.out.print("Enter new salary: ");
-        String value = input.nextLine();
+        out.print("Enter company name: ");
+        checkStringValue("company");
+        out.print("Enter key of employee: ");
+        checkStringValue("key");
+        out.print("Enter new salary: ");
+        checkStringValue("value");
         companyObj.changeSalary(company, key, value);
     }
 
     void changeP(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
-        System.out.print("Enter key of employee: ");
-        String key = input.nextLine();
-        System.out.print("Enter new position of employee: ");
-        String position = input.nextLine();
+        out.print("Enter company name: ");
+        checkStringValue("company");
+        out.print("Enter key of employee: ");
+        checkStringValue("key");
+        out.print("Enter new position of employee: ");
+        checkStringValue("position");
         companyObj.changePosition(company, key, position);
     }
 
     void deleteW(){
-        System.out.print("Enter company name: ");
-        String company = input.nextLine();
-        System.out.print("Enter key of employee: ");
-        String key = input.nextLine();
-        System.out.print("\nAre you sure? (Y/N): ");
+        out.print("Enter company name: ");
+        checkStringValue("company");
+        out.print("Enter key of employee: ");
+        checkStringValue("key");
+        out.print("\nAre you sure? (Y/N): ");
         String confirm = input.nextLine();
         if (confirm.equals("Y")) {
             companyObj.deleteWorker(company, key);
